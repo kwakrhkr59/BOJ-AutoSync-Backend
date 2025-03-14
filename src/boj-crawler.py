e402605751d0ee30fe676865c15b1fccd3e5211c
unknown <kwakrhkr59@gmail.com>
2025-03-14 15:47:44 +0900

    feat: 제출 기록 조회 함수 추가

--- src/boj-crawler.py
+++ src/boj-crawler.py
@@ -3,14 +3,13 @@ from bs4 import BeautifulSoup
 
 BASE_URL = "https://www.acmicpc.net"
 
-def login(boj_id, boj_pwd):
-    session = requests.Session()
+session = requests.Session()
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    "Referer": "https://www.acmicpc.net/"
+}
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-        "Referer": "https://www.acmicpc.net/"
-    }
-    
+def login(boj_id, boj_pwd):
     login_url = f"{BASE_URL}/signin"
     payload = {'login_user_id': boj_id,
                'login_password': boj_pwd}
@@ -18,6 +17,26 @@ def login(boj_id, boj_pwd):
     res = session.post(url=login_url, data=payload, headers=headers)
     print("res:", res)
     print("res.status_code:", res.status_code)
+    if res.status_code == 200:
+        print(boj_id, "로그인 성공")
+        return True
+    else:
+        print(res.status_code, "로그인 실패")
+        return False
+
+def getStatus(boj_id):
+    status_url = f"{BASE_URL}/status?user_id={boj_id}&result_id=4"
+    res = session.get(status_url, headers=headers)
+     
+    print("res", res)
+    print("res.status_code", res.status_code)
+    print(res.text)
+    if res.status_code == 200:
+        print(boj_id, "조회 성공")
+        return True
+    else:
+        print(res.status_code, "조회 실패")
+        return False
 
 if __name__ ==  '__main__':
     import os
@@ -27,5 +46,6 @@ if __name__ ==  '__main__':
 
     boj_id = os.getenv("BOJ_ID")
     boj_pwd = os.getenv("BOJ_PWD")
-    
-    login(boj_id, boj_pwd)
+
+    if login(boj_id, boj_pwd):
+        getStatus(boj_id)
