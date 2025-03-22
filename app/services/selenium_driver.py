f3651f8bc46decb5309a1c5f8a90079832e04d93
unknown <kwakrhkr59@gmail.com>
2025-03-22 15:49:17 +0900

    fix: selenium driver 호출 및 생성 방식 변경

--- app/services/selenium_driver.py
+++ app/services/selenium_driver.py
@@ -1,23 +1,19 @@
 from selenium import webdriver
 import time
 
-driver = None
-
 def get_driver():
-    global driver
-    if driver is None:
-        options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")  # GUI 없이 실행        
-        # options.add_argument("--disable-gpu")  
-        # options.add_argument("--window-size=1920x1080")
-        # options.add_argument("--no-sandbox")
-        # options.add_argument("--disable-dev-shm-usage")
-        # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
-        driver = webdriver.Chrome(options=options)
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")  # GUI 없이 실행        
+    # options.add_argument("--disable-gpu")  
+    # options.add_argument("--window-size=1920x1080")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+    driver = webdriver.Chrome(options=options)
     return driver
 
 def fetch_url(url):
-    get_driver()
+    driver = get_driver()
     driver.get(url)
     time.sleep(3)  # 페이지 로드 대기
 
